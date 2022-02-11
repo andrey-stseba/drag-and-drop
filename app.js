e6e23cf7b0b54740305202adf1673ec159f94254
andru <scebaandru@gmail.com>
2022-02-11 12:16:37 +0200

    refactoring function dragover

--- app.js
+++ app.js
@@ -5,35 +5,37 @@ const placeholders = document.querySelectorAll('.placeholder')
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
-function dragstart (e) {
-  e.target.classList.add('hold')
-  setTimeout(() => e.target.classList.add('hide'), 0)
+function dragstart(e) {
+    e.target.classList.add('hold')
+    setTimeout(() => e.target.classList.add('hide'), 0)
 }
-function dragend (e) {
-  e.target.className = 'item'
+
+function dragend(e) {
+    e.target.className = 'item'
 }
 
 for (const placeholder of placeholders) {
-  placeholder.addEventListener('dragover', dragover)
+    console.log(placeholder);
+    // placeholder.addEventListener('dragover', dragover)
 }
 
-function dragover (e) {
-  console.log('over', e)
-  // e.preventDefault()
+function dragover(e) {
+    console.log('over', e)
+        // e.preventDefault()
 }
 
-function dragenter (e) {
-  console.log('enter')
-  // e.target.classList.add('hovered')
+function dragenter(e) {
+    console.log('enter')
+        // e.target.classList.add('hovered')
 }
 
-function dragleave (e) {
-  console.log('le')
-  // e.target.classList.remove('hovered')
+function dragleave(e) {
+    console.log('le')
+        // e.target.classList.remove('hovered')
 }
 
-function dragdrop (e) {
-  console.log('drop')
-  // e.target.classList.remove('hovered')
-  // e.target.append(item)
+function dragdrop(e) {
+    console.log('drop')
+        // e.target.classList.remove('hovered')
+        // e.target.append(item)
 }
